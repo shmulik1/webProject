a6e2026eb48908b868da8a5f33e8844a7706f1e3
shmulik1 <skrayten@gmail.com>
2016-11-17 22:04:08 +0200

    Update 17/11/2016

--- lab/public/script.js
+++ lab/public/script.js
@@ -3,7 +3,43 @@
     // create the module and name it scotchApp
         // also include ngRoute for all our routing needs
     var scotchApp = angular.module('scotchApp', ['ngRoute']);
+	var currentUser = {
+		userName:"",
+		password:""
+	};
+	window.isManeger = false;
 
+	var addUser = function(){
+		var newUser = {
+			fullName: document.getElementById("fullN").value,
+			userName: document.getElementById("userN").value,
+			password: document.getElementById("passW").value,
+			numBranch: document.getElementById("bNum").value,
+			userType: document.getElementById("uType").value
+		}  
+	}
+	
+	var logIn = function(){
+		currentUser.userName = document.getElementById("uname").value;
+		currentUser.password = document.getElementById("pwd").value; 
+		$.ajax({ url:"http://localhost:5557/Login",
+                type: "POST",     
+                data:{
+                userName: currentUser.userName,
+                password: currentUser.password
+            },
+            success: function(data){
+            if(-1 !== data.indexOf("מנהל")){
+                isManeger = true;
+                }
+            },
+            error: function(e){
+                
+            }
+        })               
+    }
+	
+	
     // configure our routes
     scotchApp.config(function($routeProvider) {
         $routeProvider
@@ -20,15 +56,15 @@
             })
 
             // route to user_Management page
-            .when('/user_Management', {
+            $routeProvider.when('/user_Management', {
                 templateUrl : 'pages/user_Management.html',
-                controller  : 'accountController'
+                controller  : 'user_ManagementController'
             })
 			// route to login page
-			 .when('/login', {
-                templateUrl : 'pages/login.html',
-                controller  : 'loginController'
-            })
+			// .when('/login', {
+            //    templateUrl : 'pages/login.html',
+            //   controller  : 'loginController'
+            //})
 			// route to about page
 			.when('/about', {
                 templateUrl : 'pages/about.html',
@@ -71,11 +107,15 @@
 	
     });
 	var userslist = [];
-    scotchApp.controller('accountController',function($scope,$http) {
-		$http.get("http://localhost:5557/userslist").success(function(data){
+    scotchApp.controller('user_ManagementController',function($scope,$http) {
+		if(isManeger){
+			$http.get("http://localhost:5557/userslist").success(function(data){
 			$scope.userslist = data;
 		});
-		
+		}
+        else{
+            alert("לא חמוד");
+        }
     });
 
 	
